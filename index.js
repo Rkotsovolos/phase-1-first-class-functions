1ffec40b77f44abc884947e26bfa8ca13deeb271
Ryan Kotsovolos <rkotsovolos@gmail.com>
2021-10-13 17:07:55 -0700

    commit tree d7df460829f4a131d947467c74448afe47127211

---
+++ index.js
@@ -0,0 +1,19 @@
+function receivesAFunction(something) {
+    something();
+}
+
+
+function returnsANamedFunction() {
+    return returnsANamedFunction;
+}
+returnsANamedFunction();
+    
+
+let returnsAnAnonymousFunction = function()  {
+    return function(){
+        
+    };
+}
+
+
+
